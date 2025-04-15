29f377eb006dd066d70984fc6d3531c673604a99
EvanLadeira <150617090+EvanLadeira@users.noreply.github.com>
2025-04-15 16:26:53 +0200

    flemme

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,30 +3,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 100f;
-    public float zFixe = 20f;
+    public float speed;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      speed = 1000f;
     }
 
     // Update is called once per frame
     void Update()
     {     
-        // Récupère la position de la souris en pixels et ajoute une profondeur Z
-        Vector3 sourisEcran = Input.mousePosition;
-        sourisEcran.z = Mathf.Abs(Camera.main.transform.position.z); // ou une autre profondeur si tu veux
-
-        // Convertir en position dans le monde
-        Vector3 cible = Camera.main.ScreenToWorldPoint(sourisEcran);
-
-        // Fixer la valeur de Z
-        cible.z = 0f; // ou toute autre valeur que tu veux fixer
-
-        // Déplacer l'objet vers la position de la souris
-        this.transform.position = Vector3.MoveTowards(this.transform.position, cible, speed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public int score_total = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(Update_Score());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator Update_Score()
+    {
+        while(true)
+        {
+            score_total++;
+            print(score_total);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
