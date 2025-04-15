f1ef8c753bedb414deb1a3307cccacfed6f7ff87
EvanLadeira <150617090+EvanLadeira@users.noreply.github.com>
2025-04-15 15:39:04 +0200

    jsp

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -15,26 +15,7 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        /*if(Input.GetKey(KeyCode.W))
-        {
-            this.transform.position += Vector3.up * (speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            this.transform.position += Vector3.down * (speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            this.transform.position += Vector3.left * (speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            this.transform.position += Vector3.right * (speed * Time.deltaTime);
-        }*/
-
-        // this.transform.position = Vector3.MoveTowards(this.transform.position, Input.mousePosition, speed * Time.deltaTime);
-
+    {     
         // Récupère la position de la souris en pixels et ajoute une profondeur Z
         Vector3 sourisEcran = Input.mousePosition;
         sourisEcran.z = Mathf.Abs(Camera.main.transform.position.z); // ou une autre profondeur si tu veux
