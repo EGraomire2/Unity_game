bfd7f66eae69b467c240b7ecf5fe22d635acd717
EvanLadeira <150617090+EvanLadeira@users.noreply.github.com>
2025-04-15 17:19:25 +0200

    j'ai oublié ce que j'ai fait tbh

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -17,18 +17,22 @@ public class PlayerController : MonoBehaviour
     {     
         if(Input.GetKey(KeyCode.W))
         {
+            this.transform.Rotate(-1000 * Time.deltaTime, 0, 0);
             this.transform.position += Vector3.up * speed * Time.deltaTime;
         }
         if(Input.GetKey(KeyCode.S))
         {
+            this.transform.Rotate(1000 * Time.deltaTime, 0, 0);
             this.transform.position += Vector3.down * speed * Time.deltaTime;
         }
         if(Input.GetKey(KeyCode.A))
         {
+            this.transform.Rotate(0, 0, 1000 * Time.deltaTime);
             this.transform.position += Vector3.left * speed * Time.deltaTime;
         }
         if(Input.GetKey(KeyCode.D))
         {
+            this.transform.Rotate(0, 0, -1000 * Time.deltaTime);
             this.transform.position += Vector3.right * speed * Time.deltaTime;
         }
     }
