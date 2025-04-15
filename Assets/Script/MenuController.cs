2c5630e21a7536eb3ffcf042c75582a71580993e
EGromaire <etienne.gromaire77@orange.fr>
2025-04-16 01:15:01 +0200

    ASTEROIDS

--- Assets/Script/MenuController.cs
+++ Assets/Script/MenuController.cs
@@ -1,42 +1,10 @@
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public bool inMenu = false;
-    public GameObject menuPause;
-
-    public void Start()
+    public void LoadGame()
     {
-        menuPause.SetActive(false);
-    }
-
-    public void Update()
-    {
-        // Permet de mettre en pause avec Echap
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            inMenu = !inMenu;
-            menuPause.SetActive(inMenu);
-        }
-    }
-
-    // Permet de relancer le jeu de 0
-    public void RestartGame()
-    {
-        SceneManager.LoadScene("game");
-    }
-
-    // Permet de revenir au menu principal  
-    public void QuitGame()
-    {
-        SceneManager.LoadScene("menu");
-    }
-
-    // Permet de continuer le jeu
-    public void ContinueGame()
-    {
-        menuPause.SetActive(false);
+        SceneManager.LoadScene("GameScene");
     }
 }
