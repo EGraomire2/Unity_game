ffb0f90dfa20ccd62fa47529edbfa79d4fe7987f
EvanLadeira <150617090+EvanLadeira@users.noreply.github.com>
2025-04-16 14:38:21 +0200

    + map

---
+++ Assets/Script/BonusController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BonusController : MonoBehaviour
+{
+    public GameObject menuManager;
+    public float BonusVelocity;
+    private float RealVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.BonusVelocity = 100f;
+        float randomSpeedness = UnityEngine.Random.value;
+        RealVelocity = BonusVelocity + randomSpeedness * (BonusVelocity / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!menuManager.GetComponent<MenuController>().inMenu)
+        {
+            // Move the asteroid
+            this.transform.Translate(0, 0, - RealVelocity * Time.deltaTime, Space.World);
+            
+            if (this.transform.position.z <= -200){
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision){
+        print("Collision");
+        if (collision.gameObject.tag == "Player"){
+            print("game over");
+            menuManager.GetComponent<MenuController>().GameOver();
+        }
+    }
+}
--- Assets/Script/MenuController.cs
+++ Assets/Script/MenuController.cs
@@ -28,13 +28,13 @@ public class MenuController : MonoBehaviour
     public void RestartGame()
     {
         inMenu = false;
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("game");
     }
 
     // Permet de revenir au menu principal  
     public void QuitGame()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("menu");
     }
 
     // Permet de continuer le jeu
